8ccf7e28e243ef18dbefffe46587e2f0a9eb5214
Charles Maresh <cmaresh@gmail.com>
2018-03-05 12:56:53 -0600

    Make `open` function MacOS aware

--- fish/functions/open.fish
+++ fish/functions/open.fish
@@ -1,3 +1,8 @@
 function open --wraps xdg-open --description "Opens a file using the preferred app"
-  xdg-open $argv
+  switch (uname -s)
+    case Darwin
+      command open $argv
+    case '*'
+      xdg-open $argv
+  end
 end
